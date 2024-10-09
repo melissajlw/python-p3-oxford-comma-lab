fd8e7e05339223562e3d045b821f77dd551b42eb
Melissa Wang <melissa.wang21@gmail.com>
2024-10-08 23:29:39 -0400

    oxford comma lab complete

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,13 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    
+    tmp = ""
+    for item in items:
+        if item != items[-1]:
+            tmp +=  f"{item}, "
+        else:
+            tmp += f"and {item}"
+    return tmp
